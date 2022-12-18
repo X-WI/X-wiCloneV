c9d1a01a827ba068345ae13ae51ecd6456b4e792
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-19 02:21:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,17 +7,17 @@ import logging
 
 class Config:
     
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
-    BOT_SESSION = os.environ.get("BOT_SESSION", "bot") 
-    CAPTION = os.environ.get("CAPTION", "")
-    FROM_CHANNEL = os.environ.get("FROM_CHANNEL", None)
-    FILTER_TYPE = os.environ.get("FILTER_TYPE", "")
-    OWNER_ID = os.environ.get("OWNER_ID", 12345)
-    LIMIT = int(os.environ.get("LIMIT", "25000"))
-    SKIP_NO = int(os.environ.get("SKIP_NO", "0"))
-    SESSION = os.environ.get("SESSION")
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", 12345))
+    API_ID = 28298577
+    API_HASH = "143efc9236a955d630ede2642578b251"
+    BOT_TOKEN = "5068899226:AAE4Mvrssd7mE3MqaimG8ziE32edmr_Mgh0"
+    BOT_SESSION = "bot" 
+    CAPTION = None
+    FROM_CHANNEL = "@bdbdhdhvh"
+    FILTER_TYPE = "document"
+    OWNER_ID = 1903280447
+    LIMIT = 25000
+    SKIP_NO = 0
+    SESSION = "BQBWwwbTREQ6bu1Opb_kHsRqpjvYZsw34NXM--FFIWaKIvIGUl4aq269KKGgxw7b7bRsIZ8gsLkRISDMnYT3xrbWS82PvR_PV3rL_bmuz_OeNFsa_Iql91cQM9FA4z1Huv_68sarER3NSCizwC2QkL4QtbmQ21bxZCMnpeb8iYxb-yBGkaqSqXWxbUzTUCABY74pcCr46HG9comat6Z-6IfwRaHgEprkDzjGkBXUFFcmoLde75qtKXMRpA77xYN_uI2qRekBcQKnmRXsGoI9Mmh4Q4DkKWzFpTF9c57VaxNdzvrRVw_ROSC5kz84iGdpF91D-BQ-5z28lcfi0VwCZ8b_AAAAAWHA4M8A"
+    TO_CHANNEL = -1001641840781
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
