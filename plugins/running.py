8dc4973c8dbb2c60300269be4382d3e4da31d722
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-19 02:49:42 +0530

    Update running.py

--- plugins/running.py
+++ plugins/running.py
@@ -24,7 +24,7 @@ async def run(bot, message):
     )
 
     files_count = 0
-    async for message in bot.USER.search_messages(chat_id=FROM,offset=Config.SKIP_NO,limit=Config.LIMIT,filter=FILTER):
+    async for message in bot.USER.search_messages(chat_id=FROM,offset=Config.SKIP_NO,limit=Config.LIMIT,filter=FILTER.value()):
         try:
             if message.video:
                 file_name = message.video.file_name
