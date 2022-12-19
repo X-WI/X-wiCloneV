4b6316e85db599807ee2de88b9d5d8422f43d603
A - L - E - X <93909675+Lx0988@users.noreply.github.com>
2022-12-19 11:19:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     
     API_ID = 28298577
     API_HASH = "143efc9236a955d630ede2642578b251"
-    BOT_TOKEN = "5068899226:AAE4Mvrssd7mE3MqaimG8ziE32edmr_Mgh0"
+    BOT_TOKEN = "5068899226:AAEfUDZugs9bbjwNFtpKZyMoHjQjr6UTTaY"
     BOT_SESSION = "bot" 
     CAPTION = "@HQFilms4U"
     FROM_CHANNEL = "@Databaseforfiles"
