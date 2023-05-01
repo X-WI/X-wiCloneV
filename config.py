5118fea70c82c4f35c5d0f439a2982061c9eec55
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-05-02 02:01:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,12 +8,12 @@ class Config:
     BOT_TOKEN = "5068899226:AAGrSpHXOetSe-q_YSL4Sa6ghs2hhdn2Ls4"
     BOT_SESSION = "bot" 
     CAPTION = "@HQFilms4U"
-    FROM_CHANNEL = "@Databaseforfiles"
+    FROM_CHANNEL = "@ccgfdjhdjjgdsd" #https://t.me/ ccgfdjhdjjgdsd
     FILTER_TYPE = "document"
-    OWNER_ID = "1903280447"
+    OWNER_ID = "5163706369"
     LIMIT = 2
     SKIP_NO = 0
-    SESSION = "BQAbcBp7IplE9_BJIw05yKC7WZIoUDTFbnXfDLGyIYFpq76tfLLDtlqcfRyZ3rTBgtRxKrZhvsxh1DVzdhxekQuX73dmodoLM8WapEGu390jUb5vHP4y73RwgbHYFYiilMx_FFa60LHH6K1DGzhlk0dvnVn1X634zHvdpW0nmXYhMwH0nUghqSzgXPIJaE_AVHx2ZzMBv-XVluDZZJJIg2Tr2eCg8SNifCz22WJWGbM3VcGm-7VJt6S4wgoAyntndgJx9jvRvpCuOGBJ1h2YdypVh-KDRwpak_4CHubx1NTvYtDzgDzA4grO0UaO1JWURvBvl5I0zUWe2jC2IeSBgj0qAAAAAWHA4M8A"
-    TO_CHANNEL = -1001563098453
+    SESSION = "AgD6-pAAZhxCSeHVma067tyno87XQKvHC_nUoidLAYJ-qxl9_u5-u6qgAE8CxpbTrqDj1dOhy9QOkiy7zTBhsJ9c9gLFNrJx_yIeh4coPVuwNhvnmyI03JRPNR7Fl6UJhMF5l_JW2p_iNQwvIBzwmsr77WmI3KCtBgC_PV315C3wv82ZCiQhmnBQhVS-Kzyt1_3EG3wt9Be-1TRejdkdn-hN75QBT9z9slKktcUe6_sfzoYIWcDuL9yajo1NIOE3JCghHTpoazFx7jesKAIT3jbEOvYvTc0AddfWTGzFLRbOKnBPao6Lk7nToNdbpQGpkqmu1XSmWu_sO8Ghj65yQHEjMKnsXwAAAAEzx-gBAA"
+    TO_CHANNEL = -1001986761426
 def LOGGER(name: str) -> logging.Logger:
     return logging.getLogger(name)
