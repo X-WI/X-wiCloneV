92721072860a2cb1f41783871326a9e13b303065
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-05-02 02:25:20 +0530

    Update running.py

--- plugins/running.py
+++ plugins/running.py
@@ -15,19 +15,30 @@ document = enums.MessagesFilter.VIDEO
 async def run(bot, message):
     if str(message.from_user.id) not in Config.OWNER_ID:
         return
+    
+    # Get start and stop message IDs from command
+    message_text = message.text.split()
+    if len(message_text) < 3:
+        await message.reply_text("Please provide start and stop message IDs.")
+        return
+    start_id = int(message_text[1])
+    stop_id = int(message_text[2])
+    
     buttons = [[
         InlineKeyboardButton('🚫 STOP', callback_data='stop_btn')
     ]]
     reply_markup = InlineKeyboardMarkup(buttons)
     m = await bot.send_message(
-        text="<i>File Forwording Started😉</i>",
+        text="<i>File Forwarding Started😉</i>",
         reply_markup=reply_markup,
         chat_id=message.chat.id
     )
 
     files_count = 0
-    async for message in bot.search_messages(chat_id=FROM,filter=document):
+    async for message in bot.search_messages(chat_id=FROM, filter=document):
         try:
+            if message.id < start_id or message.id > stop_id:
+                continue
             if message.video:
                 file_name = message.video.file_name
             elif message.document:
@@ -59,4 +70,3 @@ async def run(bot, message):
         text=f"<u><i>Successfully Forwarded</i></u>\n\n<b>Total Forwarded Files:-</b> <code>{files_count}</code> <b>Files</b>\n<b>Thanks For Using Me❤️</b>",
         reply_markup=reply_markup
     )
-        
