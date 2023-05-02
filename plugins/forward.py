7a77f81728f89bb641161fd84f7ca8b6d7a9831d
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-05-02 14:11:53 +0530

    Update forward.py

--- plugins/forward.py
+++ plugins/forward.py
@@ -4,15 +4,17 @@ from pyrogram import Client, filters, enums
 from config import Config 
 
 media_filter = filters.document | filters.video 
+caption_text = "➠ @Hollywood_0980\n➠ @DFF_UPDATES"
 
 @Client.on_message(filters.chat(-1001667023505) & media_filter)
 async def forward(bot, update):
     try:      
+        caption_text = "➠ @Hollywood_0980\n➠ @DFF_UPDATES"
         await bot.copy_message(
             chat_id=-1001556192813, 
             from_chat_id=-1001667023505, 
             message_id=update.id, 
-            caption=f"**{update.caption}**" + '\n\n' + "**➠ @Hollywood_0980</b>\n<b>➠ @DFF_UPDATES**",          
+            caption=f"**{update.caption}**" + '\n\n' + f"**{caption_text}**",          
             parse_mode=enums.ParseMode.MARKDOWN                     
         )
     except FloodWait as e:
