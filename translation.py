b9227426698be093b228194af31e4105d1e8449c
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-05-02 12:56:19 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,12 +1,10 @@
 import os
-from config import Config
 
 class Translation(object):
   START_TXT = """<b>Hai {}!!</b>
 <i>I'm Simple Auto file Forward Bot
 This Bot forward all files from One Public channel to Your Personal channel
 More details /help</i>"""
-  CAPTION = "`{}`\n\n" + str(Config.CAPTION)
   HELP_TXT = """<b>Follow These Steps!!</b>
 <b>• Currectly fill your Heroku Config vars</b> <code>FROM_CHANNEL</code> and <code>TO_CHANNEL</code> <b>and other Vars</b>
 <b>• Then give admin permission in your personal telegram channel</b>
