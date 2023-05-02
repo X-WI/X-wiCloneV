78e2c85f3b432311ebc0789a8445e23fef787c2f
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-05-02 11:03:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,33 @@
+from pyrogram import Client, enums, __version__
 from config import Config, LOGGER
-from pyrogram import Client, __version__
+from user import User
 
-class Userbot(Client):
+
+class Bot(Client):
+    USER: User = None
+    USER_ID: int = None
+
     def __init__(self):
         super().__init__(
-            "botClient",
+            "filebot",
             api_hash=Config.API_HASH,
             api_id=Config.API_ID,
-            session_string=Config.SESSION,
-            workers=20,
             plugins={
                 "root": "plugins"
-            }
+            },
+            workers=200,
+            bot_token=Config.BOT_TOKEN            
         )
-
         self.LOGGER = LOGGER
 
     async def start(self):
         await super().start()
-        usr_bot_me = await self.get_me()
-        return (self, usr_bot_me.id)
+        bot_details = await self.get_me()
+        self.set_parse_mode(enums.ParseMode.HTML)
+        self.LOGGER(__name__).info(
+            f"@{bot_details.username}  started! "
+        )
+        self.USER, self.USER_ID = await User().start()
 
     async def stop(self, *args):
         await super().stop()
