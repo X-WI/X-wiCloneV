f4a8b7e55b1d5f15f218f50d47b8fca999b42ba5
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-05-02 10:58:14 +0530

    Update user.py

--- user.py
+++ user.py
@@ -1,1 +1,22 @@
+from config import Config, LOGGER
+from pyrogram import Client, __version__
 
+class User(Client):
+    def __init__(self):
+        super().__init__(
+            "botClient",
+            api_hash=Config.API_HASH,
+            api_id=Config.API_ID,
+            session_string=Config.SESSION,
+            workers=20
+        )
+        self.LOGGER = LOGGER
+
+    async def start(self):
+        await super().start()
+        usr_bot_me = await self.get_me()
+        return (self, usr_bot_me.id)
+
+    async def stop(self, *args):
+        await super().stop()
+        self.LOGGER(__name__).info("Bot stopped. Bye.")
