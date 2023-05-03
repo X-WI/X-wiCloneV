20ceaa16babc63a179ebb144ee961bde236b96cb
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-05-04 03:50:17 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -3,7 +3,7 @@ class Translation(object):
   START_TXT = """<b>Hai {}!!</b>
 <i>I'm Simple Auto file Forward User Bot
 This Bot forward all old videos from One channel to Your Personal channel
-More details /help</i>"""
+More details <code>/userbot_help</code></i>"""
   HELP_TXT = """<b>Follow These Steps!!</b>
 <b>• Currectly fill your Heroku Config vars</b> <code>FROM_CHANNEL</code> and <code>TO_CHANNEL</code> <b>and other Vars</b>
 <b>• Then give admin permission in your personal telegram channel</b>
@@ -12,14 +12,14 @@ More details /help</i>"""
 
 <b><u>Available Command</b></u>
 
-* /start - <b>Bot Alive</b>
-* /help - <b>more help</b>
+* /userbot_start - <b>Bot Alive</b>
+* /userbot_help - <b>more help</b>
 * /clone - <b>start forward</b>
        format = <code>/clone from_id to_id start_id end_id</code>
 
-* /about - <b>About Me</b>
+* /userbot_about - <b>About Me</b>
 
-<<b>@Lx0980AI</b>"""
+<b>@Lx0980AI</b>"""
   ABOUT_TXT = """<b><u>My Info</b></u>
 
 <b>Name :</b> <code>Auto Forward Bot</code>
@@ -27,4 +27,6 @@ More details /help</i>"""
 <b>Language :</b> <code>Python3</code>
 <b>Lybrary :</b> <code>Pyrogram v2.0.60</code>
 <b>Server :</b> <code>Heroku</code>
-<b>Build :</b><code>V0.1</code>"""
+<b>Build :</b><code>V0.1</code>
+
+<b>@Lx0980AI</b>"""
