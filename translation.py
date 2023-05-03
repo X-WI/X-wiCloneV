3bbe7311924faa452d124f8b42548d15c5eb2c74
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-05-04 00:31:14 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,21 +1,25 @@
 
 class Translation(object):
   START_TXT = """<b>Hai {}!!</b>
-<i>I'm Simple Auto file Forward Bot
-This Bot forward all files from One Public channel to Your Personal channel
+<i>I'm Simple Auto file Forward User Bot
+This Bot forward all old videos from One channel to Your Personal channel
 More details /help</i>"""
   HELP_TXT = """<b>Follow These Steps!!</b>
 <b>• Currectly fill your Heroku Config vars</b> <code>FROM_CHANNEL</code> and <code>TO_CHANNEL</code> <b>and other Vars</b>
 <b>• Then give admin permission in your personal telegram channel</b>
 <b>• Then send any message In your personal telegram channel</b>
-<b>• Then use /run command in your bot</b>
+<b>• Then use /clone command in your bot</b>
 
 <b><u>Available Command</b></u>
 
 * /start - <b>Bot Alive</b>
 * /help - <b>more help</b>
-* /run - <b>start forward</b>
-* /about - <b>About Me</b>"""
+* /clone - <b>start forward</b>
+       format = <code>/clone from_id to_id start_id end_id</code>
+
+* /about - <b>About Me</b>
+
+<<b>@Lx0980AI</b>"""
   ABOUT_TXT = """<b><u>My Info</b></u>
 
 <b>Name :</b> <code>Auto Forward Bot</code>
