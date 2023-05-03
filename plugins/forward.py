97730cf315e1c60268dcad059171d44f18cb48af
Lx 0980 <93909675+alex0980@users.noreply.github.com>
2023-05-04 01:23:06 +0530

    Create forward.py

---
+++ plugins/forward.py
@@ -0,0 +1,23 @@
+import os, asyncio, logging
+from pyrogram.errors import FloodWait
+from pyrogram import Client, filters, enums
+from config import Config 
+logger = logging.getLogger(__name__)
+
+
+media_filter = filters.document | filters.video 
+caption_text = "➠ @Hollywood_0980\n➠ @DFF_UPDATES"
+
+@Client.on_message(filters.chat(-1001667023505) & media_filter)
+async def forward(bot, update):
+    try:      
+        caption_text = "➠ @Hollywood_0980\n➠ @DFF_UPDATES"
+        await bot.copy_message(
+            chat_id=-1001556192813, 
+            from_chat_id=-1001667023505, 
+            message_id=update.id, 
+            caption=f"**{update.caption}**" + '\n\n' + f"**{caption_text}**",          
+            parse_mode=enums.ParseMode.MARKDOWN                     
+        )
+    except FloodWait as e:
+        await asyncio.sleep(e.value)
